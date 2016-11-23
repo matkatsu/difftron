45cf6e95a8991d176c0ecb177637d60607f489d4
Shota Matsushita <romiogaku@me.com>
2016-11-23 16:19:38 +0900

    debouncingについてコメント

--- App/Sagas/InputSaga.js
+++ App/Sagas/InputSaga.js
@@ -6,7 +6,13 @@ import Types from '../Actions/Types';
 import Actions from '../Actions/Creators';
 import createDiff from '../Services/CalculateDiff';
 
+// 間引き時間
+// const debouncing = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 function* worker() {
+  // Debouncing（間引き）処理
+  // yield call(debouncing, 1000);
+
   // 現在のstateを取得
   const leftInput = yield select(left);
   const rightInput = yield select(right);
@@ -17,6 +23,16 @@ function* worker() {
 }
 
 export function* leftWatcher() {
+  /* パフォーマンスに影響が出るようならdebouncingする
+  let task;
+  while (true) {
+    yield take(Types.INPUT_LEFT_CHANGE);
+    if (task) {
+      yield cancel(task);
+    }
+    task = yield fork(worker);
+  }
+  */
   yield* takeEvery(Types.INPUT_LEFT_CHANGE, worker);
 }
 export function* rightWatcher() {
